f4dc1bdd690f87a0080b22ed2701b20792a60aa2
soyeonKim <ese2003@naver.com>
2018-07-31 02:00:09 +0900

    edited Idea tab

--- moongae/IdeaViewController.swift
+++ moongae/IdeaViewController.swift
@@ -24,30 +24,29 @@ class IdeaViewController: UIViewController, ModernSearchBarDelegate {
     var projectList: Array<String> = []
     
     var isClicked:Bool = false
-    
+    var btn: UIButton = UIButton(type: .custom)
     override func viewDidLoad() {
         super.viewDidLoad()
-        // search bar
-        // 프로젝트 모델에서 태그 가져오기
-        tagList = modelProject.searchProjectTag()
         
+        // search bar
+        tagList = modelProject.searchProjectTag()
         var suggestionList = Array<ModernSearchBarModel>()
         for i in 1 ..< tagList.count {
-            suggestionList.append(ModernSearchBarModel(title:tagList[i] as! String, url:""))
+            suggestionList.append(ModernSearchBarModel(title:tagList[i], url:""))
         }
         self.searchBar.setDatasWithUrl(datas: suggestionList)
         
+        // tag
+        tagView.addTags(tagList)
+        tagView.textFont = UIFont.systemFont(ofSize: 18)
+        
+        // sphereView
         sphereView = DBSphereView(frame: CGRect(x: -200, y: -200, width: 1000, height: 1000))
         let array = NSMutableArray(capacity: 0)
-        
         for i in 1 ..< tagList.count {
-            // tag 설정
-            tagView.addTag(tagList[i] as! String)
-            tagView.textFont = UIFont.systemFont(ofSize: 15)
-            
             // btn 설정
             let btn: UIButton = UIButton(type: .custom)
-            btn.setTitle(tagList[i] as? String, for: UIControlState())
+            btn.setTitle(tagList[i], for: UIControlState())
             btn.setTitleColor(UIColor.black, for: .normal);
             btn.titleLabel?.font = UIFont.systemFont(ofSize: 18)
             btn.titleEdgeInsets = UIEdgeInsets(top: 0, left: 0 , bottom: -10, right: 0)
@@ -58,8 +57,8 @@ class IdeaViewController: UIViewController, ModernSearchBarDelegate {
             btn.layer.cornerRadius = 30
             
             // 태그 구름 클릭 시 프로젝트가 보임
-            // 프로젝트가 보이는 상태에서 클릭하면 없어져야 함.
             if(!isClicked) {
+                sphereView.timerStop()
                 btn.addTarget(self, action: #selector(IdeaViewController.buttonPressed(_:)), for: UIControlEvents.touchUpInside)
             }
             else {sphereView.removeFromSuperview()}
@@ -67,28 +66,30 @@ class IdeaViewController: UIViewController, ModernSearchBarDelegate {
             sphereView.addSubview(btn)
         }
         
+        btn.addTarget(self, action: #selector(IdeaViewController.buttonPressed(_:)), for: UIControlEvents.touchUpInside)
+        
         sphereView.setCloudTags(array as [AnyObject])
         sphereView.backgroundColor = UIColor.white
         self.view.addSubview(sphereView)
         self.view.bringSubview(toFront: tagView)
         self.view.bringSubview(toFront: searchBar)
         
+        // 배경 클릭 시 프로젝트 구름 사라짐
         let gesture = UITapGestureRecognizer(target: self, action: #selector(IdeaViewController.someAction(_:)))
         self.sphereView.addGestureRecognizer(gesture)
     }
     
-    // 프로젝트에 대한 버튼
+    // 태그 버튼 눌렀을 때
     @objc func buttonPressed(_ btn: UIButton) {
         isClicked = true
         sphereView.timerStop()
-        
         projectList = modelProject.searchProjectNameOfTag(tagName: (btn.titleLabel?.text)!)
         
+        // 프로젝트 나오는 부분
         UIView.animate(withDuration: 0.5, animations: {() -> Void in
             btn.transform = CGAffineTransform(scaleX: 3, y: 3)
-            
-            // 클릭한 버튼을 중심으로 view가 새로 생성되도록
-            self.sphereView = DBSphereView(frame: CGRect(x: (btn.frame.origin.x-200), y: (btn.frame.origin.y-200), width: 300, height: 300))
+            // 클릭한 버튼을 중심으로 view가 새로 생성됨
+            self.sphereView = DBSphereView(frame: CGRect(x: (btn.frame.origin.x-150), y: (btn.frame.origin.y-150), width: 300, height: 300))
             self.sphereSmallView = self.sphereView
             
             let array = NSMutableArray(capacity: 0)
@@ -109,9 +110,8 @@ class IdeaViewController: UIViewController, ModernSearchBarDelegate {
             
                 btn.addTarget(self, action: #selector(IdeaViewController.projectPressed(_:)), for: UIControlEvents.touchUpInside)
                 array.add(btn)
-                self.sphereView.addSubview(btn)
+                self.sphereSmallView.addSubview(btn)
             }
-            
             self.sphereView.setCloudTags(array as [AnyObject])
             self.view.addSubview(self.sphereView)
             self.view.bringSubview(toFront: self.tagView)
@@ -120,8 +120,7 @@ class IdeaViewController: UIViewController, ModernSearchBarDelegate {
             let gesture = UITapGestureRecognizer(target: self, action: #selector(IdeaViewController.someAction(_:)))
             self.sphereView.addGestureRecognizer(gesture)
         }) {
-            (finished) -> Void in
-            UIView.animate(withDuration: 0.3, animations: { () -> Void in
+            (finished) -> Void in UIView.animate(withDuration: 0.3, animations: {() -> Void in
                 btn.transform = CGAffineTransform(scaleX: 1, y: 1)
             }, completion: { (finished) -> Void in
                 self.sphereView.timerStart()
@@ -132,14 +131,18 @@ class IdeaViewController: UIViewController, ModernSearchBarDelegate {
     @objc func someAction(_ sender:UITapGestureRecognizer){
         if(isClicked) {
             self.sphereSmallView.isHidden = true
-            self.sphereView.timerStart()
+            sphereView.timerStop()
         }
-        
+        isClicked = false
     }
     
     // 프로젝트 선택 시
     // 해당 프로젝트로 이동해야함
     @objc func projectPressed(_ btn: UIButton) {
+        let storyboard: UIStoryboard = UIStoryboard(name: "Collection", bundle: nil)
+        let nextView = storyboard.instantiateInitialViewController()
+        self.present(nextView!, animated: true, completion: nil)
+        
         print("\(String(describing: btn.titleLabel!.text))프로젝트 선택됨")
         self.sphereView.isHidden = true
         self.sphereView.timerStop()
