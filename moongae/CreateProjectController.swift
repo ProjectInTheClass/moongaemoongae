5487d4d091d83f21d10219167a06dc4240601003
YEBEENPARK <parkbin3555@naver.com>
2018-08-02 23:30:12 +0900

    마지막이다 ㅠ

--- moongae/CreateProjectController.swift
+++ moongae/CreateProjectController.swift
@@ -124,6 +124,9 @@ class CreateProjectController: UIViewController, UIImagePickerControllerDelegate
         ProjectModel.ProjectModelSingleton.addProject(title: "공간캡슐", image: filename, startDate: "2018-03-08", endDate: "2018-06-22", tags: ["자바","씨쁠쁠","안드로이드","서버"], summary: "소중한 공간 속 소중한 순간들", coworker: ["김소연", "박예빈"], language: "서버 ,java", environment: "안드로이드 스튜디오 3.1.1, SQLite", motivation: "기억을 리마인드 할 수 있는 서비스를 만들고 싶었다.", benefit: "재밌을 것 같다. "
         , createdDate:"2018-06-22",  author: "이재은", major: "컴퓨터학과")
         
+        
+        self.navigationController?.popViewController(animated: true)
+        
 //                ProjectModel.ProjectModelSingleton.addProject(title: "네번째에요", image: filename, startDate: "2018-03-08", endDate: "2018-06-22", tags: ["자바","씨쁠쁠","안드로이드"], summary: "소중한 공간 속 소중한 순간들", coworker: ["김소연", "박예빈", "이재은"], language: "서버 ,java", environment: "안드로이드 스튜디오 3.1.1, SQLite", motivation: "바쁜 현대인들을 위해....,", benefit: "재밌을 것 같다.",  detail:" ", createdDate:"2018-06-22",  author: "이재은", major: "컴퓨터학과", likeCount:127)
         // self. = self.modelComment.searchForTitle(title: labelTitle.text!)
         
